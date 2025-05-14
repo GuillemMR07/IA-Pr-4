70a6d620ecf7bd74934cacc8cdd838849f61820b
GuillemMR07 <guillem.molina@insbaixcamp.cat>
2025-05-15 00:07:08 +0200

    Update preguntes_bd.py

--- Ia basada en regles/preguntes_bd.py
+++ Ia basada en regles/preguntes_bd.py
@@ -15,12 +15,14 @@ cursor.execute("""
 
 # Dades inicials
 dades = [
-    ("horari,classes", "L'horari lectiu és de dilluns a divendres de 8:00 a 14:30."),
-    ("menjador,dinar", "El servei de menjador està disponible de 14:30 a 16:00."),
-    ("festiu,vacances", "Els festius són Nadal, Setmana Santa i estiu. Consulta el calendari al web."),
-    ("contacte,telèfon", "Pots trucar al 93 123 45 67 o escriure a institut@exemple.cat."),
+    ("horari,consergeria", "L'horari de consergeria és de 8:30 a 22:00 de dilluns a divendres."),
+    ("horari,secretaria", "L'horari de secretaria és: <br> De l'1 d'octubre al 31 de maig <br> De dilluns a divendres de 9:00 a 13:00h <br> De dilluns a dijous de 16:00 a 17:30 h"),
+    ("contacte,telèfon,mail,fax,direcció", "Aquí et deixo un mica d'informació de contacte del centre i la seva direcció: <br> Carrer Jacint Barrau,1 <br> 43201-Reus <br> Telèfon: 977310953 <br> Fax: 977314721 <br> e-mail: e3002594@xtec.cat"),
+    
     ("director,directora", "La directora actual de l'institut és la Sra. Maria Soler."),
     ("reunió,famílies", "Les reunions es comuniquen per correu o app amb antelació."),
+    ("menjador,dinar", "El servei de menjador està disponible de 14:30 a 16:00."),
+    ("festiu,vacances", "Els festius són Nadal, Setmana Santa i estiu. Consulta el calendari al web."),
 ]
 
 # Inserir dades
